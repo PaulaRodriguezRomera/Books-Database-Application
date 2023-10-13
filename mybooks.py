ade727405d8e5b30729c63046ad9b6fb2e8c6f6b
Paula Rodriguez Romera <prodrig3@ed.ac.uk>
2023-10-13 17:21:56 +0100

    installed mysql connector and configured the database

--- mybooks.py
+++ mybooks.py
@@ -1,6 +1,8 @@
 from tkinter import Tk, Button, Label, Scrollbar, Listbox, StringVar, Entry, W, E, N, S, END
 from tkinter import ttk
 from tkinter import messagebox
+from mysql_config import dbConfig
+import mysql.connector as pyo
 
 root = Tk()
 
---
+++ mysql_config.py
@@ -0,0 +1,6 @@
+dbConfig = {
+    'user': 'root',
+    'password': "mypassword",
+    'host': "localhost",
+    'database': "mybooks"
+}
